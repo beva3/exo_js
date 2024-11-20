a39c2b97235546c227f9d27de602942d6200e8e1
beva3 <mizabienvenu@gmail.com>
2024-11-20 23:34:25 +0530

    horlog ok

---
+++ clock/script.js
@@ -0,0 +1,24 @@
+class Clock{
+    constructor(elementId){
+        this.clockElement = document.getElementById(elementId)
+    }
+    // get curent time
+    getCurentTime(){
+        const now = new Date()
+        const h = now.getHours().toString().padStart(2, '0')
+        const m = now.getMinutes().toString().padStart(2, '0')
+        const s = now.getSeconds().toString().padStart(2, '0')
+        return `${h}:${m}:${s}`
+    }
+    // update clock every second
+    update(){
+        this.clockElement.textContent = this.getCurentTime()
+    }
+    start(){
+        this.update()
+        setInterval(() => this.update(), 1000)
+    }
+}
+
+const myClock = new Clock('clock')
+myClock.start()
