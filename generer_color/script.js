22cd3aefb2768e28992477b338cec0520461a4a9
beva3 <mizabienvenu@gmail.com>
2024-11-20 23:11:27 +0530

    poo generate color

--- generer_color/script.js
+++ generer_color/script.js
@@ -1,3 +1,24 @@
 // select elements
-const colorDisplay = document.getElementById('colorDisplay')
-const generBtn = document.getElementById('generateBtn')
+
+// Fonction pour générer une couleur aléatoire
+class generatColor{
+    constructor(){
+        this.colorDisplay = document.getElementById("colorDisplay")
+        this.generateBtn = document.getElementById("generateBtn")
+    }
+    generate(){
+        const random_color = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`
+        return random_color
+    }
+    apply(color){
+        document.body.style.backgroundColor = color
+        this.colorDisplay.textContent = color
+        this.colorDisplay.style.backgroundColor = color
+    }
+    initialize(){
+        this.generateBtn.addEventListener('click', () => this.apply(this.generate()))
+    }
+}
+
+const colorApp = new generatColor()
+colorApp.initialize()
