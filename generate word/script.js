ea8cc79f1d165c094a166cfb9982207a65b0b299
beva3 <mizabienvenu@gmail.com>
2024-11-23 17:56:05 +0530

    initialize JS

---
+++ generate word/script.js
@@ -0,0 +1,11 @@
+let word_box = document.getElementById('word-box');
+const words = [
+    "hello",
+    "world",
+    "javascript",
+    "programming",
+    "fun"
+];
+let index = 2;
+console.log(word_box);
+word_box.innerHTML = words[index]
