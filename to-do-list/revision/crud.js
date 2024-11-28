4650613f887c1f25fb78ef75ad3ac859f371b268
beva3 <mizabienvenu@gmail.com>
2024-11-28 22:07:15 +0530

    print client

--- to-do-list/revision/crud.js
+++ to-do-list/revision/crud.js
@@ -1,14 +1,42 @@
 class Crud{
     constructor(){
-        this.client = []
+        this.clients = [
+           {id:1,name:'John',email : 'john@example.com'},
+           {id:2,name:'Jane',email : 'jane@example.com'},
+           {id:3,name:'Bob',email : 'bob@example.com'}  //... add more clients here
+        ]
+        this.table_body = document.getElementById('table-body');
+
+        
+
+        
     }
     add(){
-        
-        this.update()
+        console.log('add client');
+        const client = {id:4,name:'John',email:'John@example.com'};
+        this.clients.push(client);
         this.print()
     }
+    get_row(client){
+        return `
+            <tr>
+                <td>${client.id}</td>
+                <td>${client.name}</td>
+                <td>${client.email}</td>
+                <td>
+                    <button class="edit-btn">Edit</button>
+                    <button class="delete-btn">Delete</button>
+                </td>
+            </tr>
+        `
+    }
     print(){
+        console.log('fafao daholo ny eo ambony');
+        
         console.log('print clients');
+        this.clients.forEach(cl =>{
+            this.table_body.innerHTML += this.get_row(cl)
+        });
     }
     update(){
         console.log('update client');
